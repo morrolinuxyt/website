3881185c729b659ff7b38537020c5a175d26037e
MrRiky54 <riky0504@gmail.com>
2016-07-05 17:56:19 +0200

    Add files via upload

---
+++ mobile/contatti.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+	<head>
+		<title>Contatti :: Morrolinux</title>
+		<link href="css/stile.css" rel="stylesheet" type="text/css">
+
+		<?php include("head.html"); ?>
+
+	</head>
+	<body>
+
+        <?php include("nav.php"); ?>
+
+		<div class="testo">
+
+			<h1>Contatti</h1>
+			<p>
+			Restiamo in contatto!<br />
+			Sulla mia pagina Facebook condivido “cose interessanti” da all over the world.
+			Mentre se non volete perdervi l’ultimo video e un po' di backstage, trovate il mio canale Telegram.
+			Twitter è consigliato per non perdersi le pubblicazioni automatiche :)<br /><br />
+			<a href="https://www.youtube.com/user/morrolinux/" target="blank_">Youtube</a><br />
+			<a href="https://www.facebook.com/MorrolinuxOfficial/" target="blank_">Facebook</a><br />
+			<a href="https://twitter.com/morrolinux/" target="blank_">Twitter</a><br />
+			<a href="https://plus.google.com/u/0/+morrolinux/" target="blank_">Google Plus</a><br />
+			<a href="https://telegram.me/morrolinux_feed" target="blank_">Telegram</a><br />
+			</p>
+
+		</div>
+	</body>
+</html>
