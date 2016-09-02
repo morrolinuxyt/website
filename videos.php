b06572dad5919ebbd24c0709af9fa0da18672377
MrRiky54 <riky0504@gmail.com>
2016-09-02 13:53:12 +0200

    Added 6 songs to videos.php

--- videos.php
+++ videos.php
@@ -13,13 +13,52 @@
    la provenienza di quest'eccellenza neoclassica.<br /><br />
    \"MASTODONTICO\" 10/10 -IGN [Questa la capiranno in pochi]"),
 
+   array("XbH1f6k2xa0", "Palline Colorate - Solange", "Solange &egrave;, stando alla sua pagina Wikipedia, un sensitivo, conduttore televisivo, cantante e attore italiano.
+   Insomma, chi non ha mai visto un sensitivo che canta? Forse nessuno, e solo ora ne capisco il motivo... questa canzone DOVREBBE essere fatta per bambini, anche se pare pi&ugrave;
+   per aspiranti suicidi, ma i gusti sono gusti... <br /><br />In realt&agrave; all'inizio mi stava pure simpatico, probabilmente per l'assonanza con &quot;Assange&quot;,
+   anche se credo che quest'ultimo stia tentando di cambiare cognome, ma poi ho capito che &egrave; un toscano e che Solange &egrave;
+   solo il suo nome d'arte, ammesso che di arte si tratti..."),
+
+   array("mslSdVUyXIk", "Il Bisogno di Dirtelo - Igino del Vomero", "Io e Igino abbiamo una cosa in comune: abbiamo entrambi una cosa da dirci.
+   E, nonostante non abbia capito cosa volesse dirmi, io gli dir&ograve; quello che gli nascondo da anni oramai...<br /><br />&quot;Igino, sei un mito!&quot;<br />
+   <br />Ok, forse non ve lo aspettavate, ma questa canzone rappresenta il nostro rapporto di stima reciproca, o almeno spero. Non ho molto da dire, anche perch&eacute;
+   poco ho capito, per&ograve; solo una cosa pero abbiate capito: io lo adoro..."),
+
+   array("1_ASLOAYUTw", "Non Sono Galline - Lorenzo e i Diamanti dell'Amore", "Non so se per mancanza di voglia o di aver fatto l'errore di aver letto i
+   commenti ma questa canzone non la commenter&ograve; io, non vi dir&ograve; io che &egrave; bella, vi convinceranno i commenti stessi:<br /><br />&quot;Maschilismo e
+   luoghi comuni, per di pi&ugrave; semi-sgrammaticati, il tutto contornato da una voce raccapricciante a dire poco. Complimenti.&quot; -Lorenzo Chiereghin<br /><br />&quot;
+   madonna questa canzone &egrave; un calcio nelle palle ben assestato...CONTINUATE COS&igrave;&#65279;&quot; -Mattia Malavasi<br /><br />&quot;Voce perfetta come il
+   ritmo :D&#65279;&quot; -AndreaLeonheart<br /><br />&quot;No, dai, seriamente?&quot; -Skorhed<br /><br />&quot;Alle mie orecchie sono cresciuti gli occhi per piangere&#65279;&quot;
+   -SlamTheGamer<br /><br />&quot;QUESTA SI CHE E VERA MUSICA ! LE SUBLIMI NOTE DI QUESTA VOCE DA UNA PROFONDIT&Agrave; ASSIEME ALLA POESIA...PER NON PARLARE DI QUESTA CHITARRA...
+   SEMPLICEMENTE STUPENDO !!!&#65279;&quot; -Luca Jones"),
+
+   array("4tUuUVLilNM", "Canta Tristezza - Michele Porro", "Avete presente quando all'inizio dell'Iliade si dice: &quot;Cantami o diva del Pelide Achille l'ira funesta ecc..
+   ecc..&quot;? Ecco, questo video pu&ograve; essere considerato di tale elevatura culturale, se non di pi&ugrave;. Nella canzone sentiamo il maestro Michele Porro
+   [Chi lo conosce sto qui?] con una canzone dove invoca non la diva ma... la tristezza! Una cosa romanticissima per descrivere i suoi sogni pi&ugrave; segreti...<br />
+   <br />Insomma Omero, si disse fosse cieco, credo che dopo aver ascoltato questa canzone volesse essere anche sordo..."),
+
+   array("yL1cVjZ3hLY", "Se Proprio - Alby", "Non mentiamoci: queste sono canzoni di una qualit&agrave; molto discutibile.<br />E allora potreste chiedervi:
+   &quot;Perch&eacute; non mi metto a fare musica anche io? Tanto peggio di questi non posso fare...&quot;<br />Un consiglio spassionato: NON FATELO, a meno
+   che non mi vogliate far crepare dalle risate...<br />Ma, per il vostro bene e la vostra sanit&agrave; mentale, non lo farei se fossi in voi.<br /><br />Cosa pu&ograve;
+   succedere di male? Questo video ne &egrave; la prova..."),
+
+   array("cB8QpS9mmfg", "Paura - Annarita Campo", "Paura: un sentimento fragile e tenebroso, che pochi vogliono provare di proposito.<br />
+   Un sentimento che arranca le fragili menti dell'uomo ma che, allo stesso tempo, ci fa vivere. <br />Un sentimento unico, pieno di passione,
+   spesso rappresentato dal colore nero, come il video di questa &quot;canzone&quot; di Annarita Campo.<br /><br />Il sentimento che ho provato &egrave;
+   esattamente la paura di uscirne pazzo, alla fine di questo video...<br /><br /><br />Paura?"),
+
+   array("660xIdzEvpg", "Buongiorno Fino A Quando Servo - Dario Margeli", "Un po' mi dispiace. Mi dispiace deridere questo cantante: in fondo &egrave;
+   un operaio che non ce la fa pi&ugrave; di lavorare...<br />Ma dopo che ho visto pure gli errori grammaticali, non ho saputo resistere.<br />Sono passato sopra la voce,
+   la musica e pure l'intonazione, ma gli errori grammaticali no, non li accetto. O meglio, s&igrave;, li accetto, per farmi una grassa risata.<br /><br />Insomma, godetevi il
+   video coi commenti disabilitati... [Mai na gioia]"),
+
    array("KuYTksZJMk4", "Miraggio - Antonio Caterina", "I tempi sono fondamentali, dovunque.<br /> Lo sono la mattina quando vi svegliate, la sera quando giocate e nella musica.
-          Devo ammetterlo, quest'uomo mi ha fatto ricredere: non credo di aver mai sentito una sinfonia più sincronizzata di questa. Antonio Caterina mi ha fatto capire che
-          se pensassimo un po' di più ai tempi la canzone verrebbe anche meglio. Sono veramente orgoglioso di aver conosciuto questo cantante dalle doti eccezionali.<br />
-          Il testo, poi, ragiona sull'importanza di combattere nella vita e sulla fondamentalità mondana dell'amore congiunto.<br />Se, invece, vogliamo commentare il video potremmo dire che da
-          ora il colore dell'amore non è più il rosso, ma il viola. La scelta della tonalità è stata presa con coraggio dal regista che ha deciso di utilizzare una risoluzione minore,
-          per far immaginare tutto e non vedere e basta (O forse avevano una videocamera del 1953 a cui era rimasto un solo colore).<br /><br />Intervistato sulla questione, Kubrick si è
-          rigirato nella tomba, e Michael Bay ha riferito alla stampa che non ci sono esplosioni, poco prima di scoppiare in lacrime.")
+   Devo ammetterlo, quest'uomo mi ha fatto ricredere: non credo di aver mai sentito una sinfonia più sincronizzata di questa. Antonio Caterina mi ha fatto capire che
+   se pensassimo un po' di più ai tempi la canzone verrebbe anche meglio. Sono veramente orgoglioso di aver conosciuto questo cantante dalle doti eccezionali.<br />
+   Il testo, poi, ragiona sull'importanza di combattere nella vita e sulla fondamentalità mondana dell'amore congiunto.<br />Se, invece, vogliamo commentare il video potremmo dire che da
+   ora il colore dell'amore non è più il rosso, ma il viola. La scelta della tonalità è stata presa con coraggio dal regista che ha deciso di utilizzare una risoluzione minore,
+   per far immaginare tutto e non vedere e basta (O forse avevano una videocamera del 1953 a cui era rimasto un solo colore).<br /><br />Intervistato sulla questione, Kubrick si è
+   rigirato nella tomba, e Michael Bay ha riferito alla stampa che non ci sono esplosioni, poco prima di scoppiare in lacrime.")
  );
 
 ?>
