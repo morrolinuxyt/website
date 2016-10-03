fc95a96f1eb52514f1284d313b7d0e50555b9220
Riccardo Carissimi <riky0504@gmail.com>
2016-10-03 20:34:50 +0200

    Fixed Bug

--- js/script.js
+++ js/script.js
@@ -8,10 +8,8 @@ $(document).ready(function(){
     var color = $(this).css("background-color");
     if (color === "rgb(205, 220, 57)") {
       $(this).css("background-color", "#E0E0E0");
-      $(this).css("opacity", "1");
     } else {
       $(this).css("background-color", "#CDDC39");
-      $(this).css("opacity", "0.8");
     }
   });
 });
