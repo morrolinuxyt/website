0264189bf05a0320ba999fe21925f12d2d3c014f
Riccardo Carissimi <riky0504@gmail.com>
2016-10-09 13:47:09 +0200

    Fixed Minor Bug in "Progetti"

--- js/script.js
+++ js/script.js
@@ -6,9 +6,8 @@ $(document).ready(function(){
     $(this).find(".to_close").slideToggle();
     $(this).find(".to_open").slideToggle();
     var color = $(this).css("background-color");
-    if (color === "rgb(205, 220, 57)") {
+    if (color === "rgb(225, 235, 127)") {
       $(this).css("background-color", "#E0E0E0");
-      $(this).css("opacity", "1");
     } else {
       $(this).css("background-color", "#CDDC39");
     }
