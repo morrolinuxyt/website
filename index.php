f395481e0eebf8e3881a918ecb7ad1321b09312c
MrRiky54 <riky0504@gmail.com>
2016-10-24 23:26:41 +0200

    Introduced Latest Video in Index

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <?php include "assets/nav.html"; ?>
 
     <div class="container">
+      <h1 style="text-align: centre;">Ultimo Video Caricato</h1>
+      <iframe width="100%" height="422" src="https://www.youtube.com/embed?listType=user_uploads&list=morrolinux" frameborder="0" allowfullscreen></iframe><br /><br />
       <?php include "assets/letter.html"; ?>
     </div>
 
