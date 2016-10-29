a1e98ac5d0a1cc657b8c0bd360b05d0b837731fc
Riccardo Carissimi <riky0504@gmail.com>
2016-10-29 13:09:40 +0200

    Added Video To LPI

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
     <div class="container">
       <h1 style="text-align: centre;">Ultimo Video Caricato</h1>
-      <iframe width="100%" height="422" src="https://www.youtube.com/embed?listType=user_uploads&list=morrolinux" frameborder="0" allowfullscreen></iframe><br /><br />
+      <iframe class="resize" src="https://www.youtube.com/embed?listType=user_uploads&list=morrolinux" frameborder="0" allowfullscreen></iframe><br /><br />
       <?php include "assets/letter.html"; ?>
     </div>
 
