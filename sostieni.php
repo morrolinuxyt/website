95ec14092ce3dea32d7723e284ff7155194fecaf
Riccardo Carissimi <riky0504@gmail.com>
2016-11-23 17:44:55 +0100

    Added "Sostieni" page

---
+++ sostieni.php
@@ -0,0 +1,32 @@
+<?php include "assets/head.html"; ?>
+
+    <title>Sostieni :: Morrolinux</title>
+
+  </head>
+  <body>
+
+    <?php include "assets/nav.html"; ?>
+
+    <div class="container">
+      <h1>Sostieni Realisticamente il Progetto</h1>
+      <p>"Morrolinux" è un progetto di <strong>divulgazione informatica libera</strong>, incentrata su soluzioni OpenSource e sicurezza.<br />
+        Per mantenere il progetto attivo e la divulgazione dei contenuti gratuita, servono aiuti e fondi.<br />
+        Anche il sito “morrolinux.it” ha un costo mensile di affitto e manutenzione, per rimanere stabile, <strong>sicuro</strong> e veloce.
+        Per offrire questi servizi, di cui tu stesso, proprio in questo momento stai usufruendo, ci affidiamo a soluzioni non sempre tradizionali.<br />
+        Ecco alcuni modi con cui puoi <strong>realmente</strong> contribuire al progetto</p>
+      <h2>Su YouTube</h2>
+      <p>-Iscrivendoti al canale<br />
+         -Disattivando eventuali <strong>AdBlock</strong> sul mio canale<br />
+         -Lasciando like e/o commenti ai video<br />
+         -Condividendo il canale e i video<br />
+      </p>
+      <h2>Sui Social</h2>
+      <p>-Seguendomi su <a href="https://www.facebook.com/MorrolinuxOfficial/" target="blank_">Facebook</a><br />
+         -Lasciando like ai post<br >
+      </p>
+      <h2>Donazioni</h2>
+      <p>Anche se i video sono visibili gratuitamente, la loro realizzazione ha dei costi, sia economici che in termini di tempo.<br />Se ti va di contribuire ad ammortizzarli e credi anche tu nell'<strong>informazione libera</strong>, puoi fare una donazione tramite il seguente link. <b>Grazie.</b>
+      <br /><br /><a style="font-weight: 300; font-size: 30px; padding: 5px 20px 5px 20px; border: solid; border-radius: 27px;" href="https://streamtip.com/y/morrolinux" target="blank_">Sostieni</a></p><br /><br />
+    </div>
+
+<?php include "assets/footer.html"; ?>
