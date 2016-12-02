9b924abea28e9d40b4d8c362fa2d332e6b055094
Riccardo Carissimi <riky0504@gmail.com>
2016-12-02 17:24:19 +0100

    Revert "Updated Site to Fully Support HTTPS!"
    This reverts commit 25817cb94931a37347d6834c0436bfff0be61058.

--- projects.php
+++ projects.php
@@ -28,11 +28,11 @@
         <h1>Wallpapers</h1>
         <p class="to_close">Sfondi per PC e Smartphone per i migliori seguaci!</p>
         <div class="to_open">
-          <img src="https://imgur.com/eMBksGe.jpg" alt="Wallpaper 1" />
-          <img src="https://imgur.com/GwCdG26.jpg" alt="Wallpaper 2" /><br /><a></a><p>Hosting By <a href="https://imgur.com/a/mILJN" target="_blank">Imgur</a></p>
-          <img src="https://orig02.deviantart.net/e0c1/f/2016/294/4/9/archblur_by_valkyagan-dalr2j5.png" alt="Wallpaper 3" /><br /><a><!-- I don't know why, but if i put a blank link before the "By" it become a text, for a bug it was a link altough there's a p tag --></a><p>By <a href="https://valkyagan.deviantart.com/" target="_blank">Valkyagan</a></p>
-          <img src="https://orig11.deviantart.net/c1ca/f/2016/247/3/4/watch_dogs2_by_valkyagan-dagh0rm.png" alt="Wallpaper 4" /><br /><p>By <a href="https://valkyagan.deviantart.com/" target="_blank">Valkyagan</a></p>
-          <img src="https://orig11.deviantart.net/561c/f/2016/233/a/4/gnulinuxwallpapergroup_by_valkyagan-daesagn.png" alt="Wallpaper 5" /><br /><p>By <a href="https://valkyagan.deviantart.com/" target="_blank">Valkyagan</a></p>
+          <img src="http://imgur.com/eMBksGe.jpg" alt="Wallpaper 1" />
+          <img src="http://imgur.com/GwCdG26.jpg" alt="Wallpaper 2" /><br /><a></a><p>Hosting By <a href="http://imgur.com/a/mILJN" target="_blank">Imgur</a></p>
+          <img src="http://orig02.deviantart.net/e0c1/f/2016/294/4/9/archblur_by_valkyagan-dalr2j5.png" alt="Wallpaper 3" /><br /><a><!-- I don't know why, but if i put a blank link before the "By" it become a text, for a bug it was a link altough there's a p tag --></a><p>By <a href="http://valkyagan.deviantart.com/" target="_blank">Valkyagan</a></p>
+          <img src="http://orig11.deviantart.net/c1ca/f/2016/247/3/4/watch_dogs2_by_valkyagan-dagh0rm.png" alt="Wallpaper 4" /><br /><p>By <a href="http://valkyagan.deviantart.com/" target="_blank">Valkyagan</a></p>
+          <img src="http://orig11.deviantart.net/561c/f/2016/233/a/4/gnulinuxwallpapergroup_by_valkyagan-daesagn.png" alt="Wallpaper 5" /><br /><p>By <a href="http://valkyagan.deviantart.com/" target="_blank">Valkyagan</a></p>
         </div>
       </div></a>
 
@@ -40,30 +40,30 @@
         <h1>Gente Simpatica</h1>
         <p class="to_close">I migliori commenti per farsi una risata!</p>
         <div class="to_open">
-          <img src="https://imgur.com/MOcyGhC.jpg" alt="Comment 1" />
-          <img src="https://imgur.com/TdrgOsW.jpg" alt="Comment 2" />
-          <img src="https://imgur.com/0vMcJUS.jpg" alt="Comment 3" />
-          <img src="https://imgur.com/oku21UK.jpg" alt="Comment 4" />
-          <img src="https://imgur.com/qUGH9w8.jpg" alt="Comment 5" />
-          <img src="https://imgur.com/mdvDS6B.jpg" alt="Comment 6" />
-          <img src="https://imgur.com/Nz6DEHs.jpg" alt="Comment 7" />
-          <img src="https://imgur.com/rHoijp7.jpg" alt="Comment 8" />
-          <img src="https://imgur.com/0CDXNw7.jpg" alt="Comment 9" />
-          <img src="https://imgur.com/paPEPwQ.jpg" alt="Comment 10" />
-          <img src="https://imgur.com/HriuwT9.jpg" alt="Comment 11"/>
-          <img src="https://imgur.com/ZubqTMJ.jpg" alt="Comment 12" />
-          <img src="https://imgur.com/K7wv6T9.jpg" alt="Comment 13" />
-          <img src="https://imgur.com/F5Kxwi7.jpg" alt="Comment 14" />
-          <img src="https://imgur.com/Vq0YGBa.jpg" alt="Comment 15" />
-          <img src="https://imgur.com/eZ7dPCT.jpg" alt="Comment 16" />
-          <img src="https://imgur.com/H1L2Z5E.jpg" alt="Comment 17" />
-          <img src="https://imgur.com/PkmWJsK.jpg" alt="Comment 18" />
-          <img src="https://imgur.com/P8FL0Rt.jpg" alt="Comment 19" />
-          <img src="https://imgur.com/qsfTNqU.jpg" alt="Comment 20" />
-          <img src="https://imgur.com/DnREfGy.jpg" alt="Comment 21" />
-          <img src="https://imgur.com/ENgiwIV.jpg" alt="Comment 22" />
-          <img src="https://imgur.com/G7mU7nx.jpg" alt="Comment 23"/>
-          <br /><a></a><p>Hosting By <a href="https://imgur.com/a/T9jg1" target="_blank">Imgur</a></p>
+          <img src="http://imgur.com/MOcyGhC.jpg" alt="Comment 1" />
+          <img src="http://imgur.com/TdrgOsW.jpg" alt="Comment 2" />
+          <img src="http://imgur.com/0vMcJUS.jpg" alt="Comment 3" />
+          <img src="http://imgur.com/oku21UK.jpg" alt="Comment 4" />
+          <img src="http://imgur.com/qUGH9w8.jpg" alt="Comment 5" />
+          <img src="http://imgur.com/mdvDS6B.jpg" alt="Comment 6" />
+          <img src="http://imgur.com/Nz6DEHs.jpg" alt="Comment 7" />
+          <img src="http://imgur.com/rHoijp7.jpg" alt="Comment 8" />
+          <img src="http://imgur.com/0CDXNw7.jpg" alt="Comment 9" />
+          <img src="http://imgur.com/paPEPwQ.jpg" alt="Comment 10" />
+          <img src="http://imgur.com/HriuwT9.jpg" alt="Comment 11"/>
+          <img src="http://imgur.com/ZubqTMJ.jpg" alt="Comment 12" />
+          <img src="http://imgur.com/K7wv6T9.jpg" alt="Comment 13" />
+          <img src="http://imgur.com/F5Kxwi7.jpg" alt="Comment 14" />
+          <img src="http://imgur.com/Vq0YGBa.jpg" alt="Comment 15" />
+          <img src="http://imgur.com/eZ7dPCT.jpg" alt="Comment 16" />
+          <img src="http://imgur.com/H1L2Z5E.jpg" alt="Comment 17" />
+          <img src="http://imgur.com/PkmWJsK.jpg" alt="Comment 18" />
+          <img src="http://imgur.com/P8FL0Rt.jpg" alt="Comment 19" />
+          <img src="http://imgur.com/qsfTNqU.jpg" alt="Comment 20" />
+          <img src="http://imgur.com/DnREfGy.jpg" alt="Comment 21" />
+          <img src="http://imgur.com/ENgiwIV.jpg" alt="Comment 22" />
+          <img src="http://imgur.com/G7mU7nx.jpg" alt="Comment 23"/>
+          <br /><a></a><p>Hosting By <a href="http://imgur.com/a/T9jg1" target="_blank">Imgur</a></p>
         </div>
       </div></a>
 
