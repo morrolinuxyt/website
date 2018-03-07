71f474aeaafb1872c8f61d7f6cbd001f72c08f3e
Riccardo Carissimi <riky0504@gmail.com>
2018-03-07 22:29:19 +0100

    Removed Some Minor Bugs

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <?php include "assets/nav.html"; ?>
 
     <div class="container">
-      <h1 style="text-align: centre;">Ultimo Video Caricato</h1>
+      <h1 style="text-align: center;">Ultimo Video Caricato</h1>
       <iframe class="resize" src="https://www.youtube.com/embed?listType=user_uploads&list=morrolinux" frameborder="0" allowfullscreen></iframe><br /><br />
       <?php include "assets/letter.html"; ?>
     </div>
