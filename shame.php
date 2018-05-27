a3f435f7f8be9024ea3bddf6f028c97e59f0bcfa
Riccardo Carissimi <riky0504@gmail.com>
2018-05-27 18:20:12 +0200

    Made "Wall of Shame" Section

---
+++ shame.php
@@ -0,0 +1,42 @@
+<?php include "assets/head.html"; ?>
+
+    <title>Wall of Shame :: Morrolinux</title>
+
+  </head>
+  <body>
+
+    <?php include "assets/nav.html"; ?>
+
+    <div class="container">
+        <h1>Wall of Shame</h1>
+        <div class="div_images">
+          <img src="https://imgur.com/MOcyGhC.jpg" alt="Comment 1" />
+          <img src="https://imgur.com/TdrgOsW.jpg" alt="Comment 2" />
+          <img src="https://imgur.com/0vMcJUS.jpg" alt="Comment 3" />
+          <img src="https://imgur.com/oku21UK.jpg" alt="Comment 4" />
+          <img src="https://imgur.com/qUGH9w8.jpg" alt="Comment 5" />
+          <img src="https://imgur.com/mdvDS6B.jpg" alt="Comment 6" />
+          <img src="https://imgur.com/Nz6DEHs.jpg" alt="Comment 7" />
+          <img src="https://imgur.com/rHoijp7.jpg" alt="Comment 8" />
+          <img src="https://imgur.com/0CDXNw7.jpg" alt="Comment 9" />
+          <img src="https://imgur.com/paPEPwQ.jpg" alt="Comment 10" />
+          <img src="https://imgur.com/HriuwT9.jpg" alt="Comment 11"/>
+          <img src="https://imgur.com/ZubqTMJ.jpg" alt="Comment 12" />
+          <img src="https://imgur.com/K7wv6T9.jpg" alt="Comment 13" />
+          <img src="https://imgur.com/F5Kxwi7.jpg" alt="Comment 14" />
+          <img src="https://imgur.com/Vq0YGBa.jpg" alt="Comment 15" />
+          <img src="https://imgur.com/eZ7dPCT.jpg" alt="Comment 16" />
+          <img src="https://imgur.com/H1L2Z5E.jpg" alt="Comment 17" />
+          <img src="https://imgur.com/PkmWJsK.jpg" alt="Comment 18" />
+          <img src="https://imgur.com/P8FL0Rt.jpg" alt="Comment 19" />
+          <img src="https://imgur.com/qsfTNqU.jpg" alt="Comment 20" />
+          <img src="https://imgur.com/DnREfGy.jpg" alt="Comment 21" />
+          <img src="https://imgur.com/ENgiwIV.jpg" alt="Comment 22" />
+          <img src="https://imgur.com/G7mU7nx.jpg" alt="Comment 23"/>
+          <img src="https://imgur.com/YrbXuqT.jpg" alt="Comment 24"/>
+          <br /><a></a><p>Hosting By <a href="https://imgur.com/a/ds6NxL9" target="_blank">Imgur</a></p>
+        </div>
+
+    </div>
+
+<?php include "assets/footer.html"; ?>
