f0f7607578ed1b442af9147475bfaa1e77fcc6c0
Riccardo Carissimi <riky0504@gmail.com>
2018-05-28 20:37:05 +0200

    Made "Wall of Shame" a Better Place
    Even for KIDS!

--- shame.php
+++ shame.php
@@ -10,26 +10,16 @@
     <div class="container">
         <h1>Wall of Shame</h1>
         <div class="div_images">
-          <img src="https://imgur.com/MOcyGhC.jpg" alt="Comment 1" />
           <img src="https://imgur.com/TdrgOsW.jpg" alt="Comment 2" />
           <img src="https://imgur.com/0vMcJUS.jpg" alt="Comment 3" />
-          <img src="https://imgur.com/oku21UK.jpg" alt="Comment 4" />
-          <img src="https://imgur.com/qUGH9w8.jpg" alt="Comment 5" />
+          <a href="http://www.youtube.com/watch?v=EN5yotLSRfo" target="_blank">↓ Come assemblare un pc potente per grafica e video editing ↓<img src="https://imgur.com/oku21UK.jpg" alt="Comment 4" /></a>
           <img src="https://imgur.com/mdvDS6B.jpg" alt="Comment 6" />
-          <img src="https://imgur.com/Nz6DEHs.jpg" alt="Comment 7" />
-          <img src="https://imgur.com/rHoijp7.jpg" alt="Comment 8" />
-          <img src="https://imgur.com/0CDXNw7.jpg" alt="Comment 9" />
-          <img src="https://imgur.com/paPEPwQ.jpg" alt="Comment 10" />
-          <img src="https://imgur.com/HriuwT9.jpg" alt="Comment 11"/>
-          <img src="https://imgur.com/ZubqTMJ.jpg" alt="Comment 12" />
-          <img src="https://imgur.com/K7wv6T9.jpg" alt="Comment 13" />
-          <img src="https://imgur.com/F5Kxwi7.jpg" alt="Comment 14" />
-          <img src="https://imgur.com/Vq0YGBa.jpg" alt="Comment 15" />
-          <img src="https://imgur.com/eZ7dPCT.jpg" alt="Comment 16" />
+          <a href="http://www.youtube.com/watch?v=1r0wPNOmkO8" target="_blank">↓ Come registrare lo schermo su Android (anche Galaxy S3) ↓<img src="https://imgur.com/Nz6DEHs.jpg" alt="Comment 7" /></a>
+          <a href="http://www.youtube.com/watch?v=tDrd3rFNIG8" target="_blank">↓ 7 Giorni a Copenaghen - i Buskers, addio Copenaghen - #DAY7 ↓<img src="https://imgur.com/HriuwT9.jpg" alt="Comment 11"/></a>
+          <a href="https://www.youtube.com/watch?v=yo7qeF1BPHU" target="_blank">↓ Linux keylogger [GUIDA e SPIEGAZIONE] ↓<img src="https://imgur.com/K7wv6T9.jpg" alt="Comment 13" /></a>
+          <a href="http://www.youtube.com/watch?v=rPajaMkkx-k" target="_blank">↓ $PATH, type, which, whereis [Certificazione LPI - 104.7] Imparare Linux ↓<img src="https://imgur.com/eZ7dPCT.jpg" alt="Comment 16" /></a>
           <img src="https://imgur.com/H1L2Z5E.jpg" alt="Comment 17" />
-          <img src="https://imgur.com/PkmWJsK.jpg" alt="Comment 18" />
-          <img src="https://imgur.com/P8FL0Rt.jpg" alt="Comment 19" />
-          <img src="https://imgur.com/qsfTNqU.jpg" alt="Comment 20" />
+          <a href="http://www.youtube.com/watch?v=jevreF-uooA" target="_blank">↓ Come cambiare alimentatore al pc [GUIDA] ↓<img src="https://imgur.com/qsfTNqU.jpg" alt="Comment 20" /></a>
           <img src="https://imgur.com/DnREfGy.jpg" alt="Comment 21" />
           <img src="https://imgur.com/ENgiwIV.jpg" alt="Comment 22" />
           <img src="https://imgur.com/G7mU7nx.jpg" alt="Comment 23"/>
