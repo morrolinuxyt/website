5b1be4445840b762100218741513f428f21276da
Riccardo Carissimi <riky0504@gmail.com>
2019-01-10 22:42:55 +0100

    Changed Text Sizes
    Changed font sizes fron "pixel" fo em
    Updated home's banner

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <div class="container">
       <!--<h1 style="text-align: center;">Ultimo Video Caricato</h1>-->
       <iframe class="resize" src="https://www.youtube.com/embed?listType=user_uploads&list=morrolinux" frameborder="0" allowfullscreen></iframe>
-      <a href="lpi.php"><div class="banner"><img src="img/right-arrow.svg" class="blink arrow"/> Novità! Tutti i corsi Linux in <b>offerta speciale</b> a € 9,99 - Clicca qui <img src="img/left-arrow.svg" class="blink arrow"/></div></a>
+      <a href="lpi.php"><div class="banner"><img src="img/right-arrow.svg" class="blink arrow"/> Novità! Tutti i corsi Linux in <b>offerta speciale</b> a 9,99€ anzichè <s>149€</s> - Clicca qui <img src="img/left-arrow.svg" class="blink arrow"/></div></a>
       <?php include "assets/letter.html"; ?>
     </div>
 
